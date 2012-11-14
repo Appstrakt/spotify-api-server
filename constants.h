71fd97746c9b9d1fec49fa678f56d4561fd336a2
stijn <stijn@appstrakt.be>
2012-11-14 15:32:22 +0100

    Added track metadata - initial draft, requires muchos refactoring and decent error handling

--- constants.h
+++ constants.h
@@ -3,6 +3,8 @@
 
 // Length of track uri
 static const int kTrackLinkLength = sizeof("spotify:track:58PipbkYEkKFzOowRPHF3m");
+static const int kAlbumLinkLength = sizeof("spotify:album:0745mDdMqet9J5nO5x7IQS");
+static const int kArtistLinkLength = sizeof("spotify:artist:0oSGxfWSnnOXhD2fKuz2Gy");
 
 // Maximum number of characters in a playlist title
 static const int kMaxPlaylistTitleLength = 256;
